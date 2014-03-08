8a0498f25ce3e5ac35f82d24a1cfb839e06c2f26
Hassan J. Ndow <hjn5906@rit.edu>
2014-03-07 23:06:14 -0500

    Hassan adds turns to player 1 and player 2

--- Breakthrough.java
+++ Breakthrough.java
@@ -13,7 +13,7 @@ public class Breakthrough extends JFrame implements ActionListener
    private int x2 = 0;
    private int x3 = 7;
    private int x4 = 7;
-
+   private boolean turn = false;
    private int y = 0;
    private int y2 = 7;
    private boolean found = false;
@@ -136,7 +136,7 @@ public class Breakthrough extends JFrame implements ActionListener
       
       try
       { 
-         //Piece Moves
+         //Piece Moves -- loops through each [row][column]
          outerLoop:
          for (int rows = 0; rows < gridUnits.length; rows++)
          {
@@ -145,92 +145,100 @@ public class Breakthrough extends JFrame implements ActionListener
             {
                
                
-               
-               //saves location of X piece clicked on
-               if(choice == gridUnits[rows][cols] && (gridUnits[rows][cols].getText().equals("X")))
+               if(turn == false)
                {
-                  x = rows;
-                  x3 = rows;
-                  y = cols;
+                  //saves location of X piece clicked on
+                  if(choice == gridUnits[rows][cols] && (gridUnits[rows][cols].getText().equals("X")))
+                  {
+                     x = rows;
+                     x3 = rows;
+                     y = cols;
+                     
                   
-               
-               }
-
-               //moves x piece horizontally
-               if(choice == gridUnits[x][y+1] && (gridUnits[x][y].getText().equals("X")) && (gridUnits[x][y+1].getText().equals("")))
-               {
-                  gridUnits[x][y].setText("");
-                  gridUnits[x][y+1].setText("X");
-                 
-                  break outerLoop;                 
-               }
-               
-               //moves x piece diagonally(right + up)
-               if(x3 > 0)
-               {
-                  if(choice == gridUnits[x3-1][y+1] && (gridUnits[x][y].getText().equals("X")) && ((gridUnits[x3-1][y+1].getText().equals("")) || (gridUnits[x3-1][y+1].getText().equals("O"))))
+                  }
+   
+                  //moves x piece horizontally
+                  if(choice == gridUnits[x][y+1] && (gridUnits[x][y].getText().equals("X")) && (gridUnits[x][y+1].getText().equals("")))
                   {
                      gridUnits[x][y].setText("");
-                     gridUnits[x3-1][y+1].setText("X");
-                    
+                     gridUnits[x][y+1].setText("X");
+                     turn = true;
                      break outerLoop;                 
                   }
-               }
-               
-               //moves x piece diagonally(right + down)
-               if(x<7)
-               {
-                  if(choice == gridUnits[x+1][y+1] && (gridUnits[x][y].getText().equals("X")) && ((gridUnits[x+1][y+1].getText().equals("")) || (gridUnits[x+1][y+1].getText().equals("O"))))
+                  
+                  //moves x piece diagonally(right + up)
+                  if(x3 > 0)
                   {
-                     gridUnits[x][y].setText("");
-                     gridUnits[x+1][y+1].setText("X");
-                    
-                     break outerLoop;                 
+                     if(choice == gridUnits[x3-1][y+1] && (gridUnits[x][y].getText().equals("X")) && ((gridUnits[x3-1][y+1].getText().equals("")) || (gridUnits[x3-1][y+1].getText().equals("O"))))
+                     {
+                        gridUnits[x][y].setText("");
+                        gridUnits[x3-1][y+1].setText("X");
+                        turn = true;
+                        break outerLoop;                 
+                     }
                   }
+                  
+                  //moves x piece diagonally(right + down)
+                  if(x<7)
+                  {
+                     if(choice == gridUnits[x+1][y+1] && (gridUnits[x][y].getText().equals("X")) && ((gridUnits[x+1][y+1].getText().equals("")) || (gridUnits[x+1][y+1].getText().equals("O"))))
+                     {
+                        gridUnits[x][y].setText("");
+                        gridUnits[x+1][y+1].setText("X");
+                        turn = true;
+                        break outerLoop;                 
+                     }
+                  }
+               
                }
 
 
                
-               
-               //save location of O piece clicked on
-               if(choice == gridUnits[rows][cols] && (gridUnits[rows][cols].getText().equals("O")))
+               if(turn == true)
                {
-                  x2 = rows;
-                  x4 = rows;
-                  y2 = cols;
- 
-               }
-
-               //moves o piece horizontally
-               if(choice == gridUnits[x2][y2-1] && (gridUnits[x2][y2].getText().equals("O")) && (gridUnits[x2][y2-1].getText().equals("")))
-               {
-                  gridUnits[x2][y2].setText("");
-                  gridUnits[x2][y2-1].setText("O");
-                  break outerLoop;
-               
-               }
-               
-               //moves O piece diagonally(right + up)
-               if(x4 > 0)
-               {
-                  if(choice == gridUnits[x4-1][y2-1]  && (gridUnits[x2][y2].getText().equals("O")) && ((gridUnits[x4-1][y2-1].getText().equals("")) || (gridUnits[x4-1][y2-1].getText().equals("X"))))
+                  //save location of O piece clicked on
+                  if(choice == gridUnits[rows][cols] && (gridUnits[rows][cols].getText().equals("O")))
+                  {
+                     x2 = rows;
+                     x4 = rows;
+                     y2 = cols;
+    
+                  }
+   
+                  //moves o piece horizontally
+                  if(choice == gridUnits[x2][y2-1] && (gridUnits[x2][y2].getText().equals("O")) && (gridUnits[x2][y2-1].getText().equals("")))
                   {
                      gridUnits[x2][y2].setText("");
-                     gridUnits[x4 - 1][y2-1].setText("O");
+                     gridUnits[x2][y2-1].setText("O");
+                     turn = false;
                      break outerLoop;
                   
                   }
-               }
-               
-               //moves O piece diagonally(right + down)
-               if(x2 < 7)
-               {
-                  if(choice == gridUnits[x2+1][y2-1]  && (gridUnits[x2][y2].getText().equals("O")) && ((gridUnits[x2+1][y2-1].getText().equals("")) || (gridUnits[x2+1][y2-1].getText().equals("X"))))
-                  {
-                     gridUnits[x2][y2].setText("");
-                     gridUnits[x2+1][y2-1].setText("O");
-                     break outerLoop;
                   
+                  //moves O piece diagonally(right + up)
+                  if(x4 > 0)
+                  {
+                     if(choice == gridUnits[x4-1][y2-1]  && (gridUnits[x2][y2].getText().equals("O")) && ((gridUnits[x4-1][y2-1].getText().equals("")) || (gridUnits[x4-1][y2-1].getText().equals("X"))))
+                     {
+                        gridUnits[x2][y2].setText("");
+                        gridUnits[x4 - 1][y2-1].setText("O");
+                        turn = false;
+                        break outerLoop;
+                     
+                     }
+                  }
+                  
+                  //moves O piece diagonally(right + down)
+                  if(x2 < 7)
+                  {
+                     if(choice == gridUnits[x2+1][y2-1]  && (gridUnits[x2][y2].getText().equals("O")) && ((gridUnits[x2+1][y2-1].getText().equals("")) || (gridUnits[x2+1][y2-1].getText().equals("X"))))
+                     {
+                        gridUnits[x2][y2].setText("");
+                        gridUnits[x2+1][y2-1].setText("O");
+                        turn = false;
+                        break outerLoop;
+                     
+                     }
                   }
                }
 
